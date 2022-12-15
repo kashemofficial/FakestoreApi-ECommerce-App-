23ac2f91f1641340128db5cd47ce7f8156fd6349
kashemofficial <poran143u@gmail.com>
2022-12-15 19:36:50 +0600

    extension

---
+++ FakestoreApiEcommerce/FakestoreApiEcommerce/Extension/AlertExtension.swift
@@ -0,0 +1,37 @@
+//
+//  AlertExtension.swift
+//  FakestoreApiEcommerce
+//
+//  Created by Abul Kashem on 15/12/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController{
+    
+    // Global Alert
+    // Define Your number of buttons, styles and completion
+    public func openAlert(title: String,
+                          message: String,
+                          alertStyle:UIAlertController.Style,
+                          actionTitles:[String],
+                          actionStyles:[UIAlertAction.Style],
+                          actions: [((UIAlertAction) -> Void)]){
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: alertStyle)
+        for(index, indexTitle) in actionTitles.enumerated(){
+            let action = UIAlertAction(title: indexTitle, style: actionStyles[index], handler: actions[index])
+            alertController.addAction(action)
+        }
+        self.present(alertController, animated: true)
+    }
+}
+
+
+
+
+
+
+
+
---
+++ FakestoreApiEcommerce/FakestoreApiEcommerce/Extension/ValidationExtension.swift
@@ -0,0 +1,36 @@
+//
+//  ValidationExtension.swift
+//  FakestoreApiEcommerce
+//
+//  Created by Abul Kashem on 15/12/22.
+//
+
+import Foundation
+
+extension String{
+    func validateEmailId() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        return applyPredicateOnRegex(regexStr: emailRegEx)
+    }
+    
+    func validatePassword(mini: Int = 7, max: Int = 7) -> Bool {
+        //Minimum 8 characters at least 1 Alphabet and 1 Number:
+        var passRegEx = ""
+        if mini >= max{
+            passRegEx = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{\(mini),}$"
+        }else{
+            passRegEx = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{\(mini),\(max)}$"
+        }
+        return applyPredicateOnRegex(regexStr: passRegEx)
+    }
+    
+    func applyPredicateOnRegex(regexStr: String) -> Bool{
+        let trimmedString = self.trimmingCharacters(in: .whitespaces)
+        let validateOtherString = NSPredicate(format: "SELF MATCHES %@", regexStr)
+        let isValidateOtherString = validateOtherString.evaluate(with: trimmedString)
+        return isValidateOtherString
+    }
+
+}
+
+
